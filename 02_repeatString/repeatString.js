eb67afcc0e5479296ceaf41eb1de0bec18a4abb2
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2023-08-19 13:21:41 +0530

    reapt string done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let ans = "";
+    if(num<0){
+        return "Error"
+    }else {
+        for(let i = 1;i<=num;i++){
+            ans +=string;
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
