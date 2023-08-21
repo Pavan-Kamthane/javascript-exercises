426f3628d4d0056708743f38da4a180ba340eb3f
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2023-08-21 22:24:36 +0530

    all done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let ans =0;
+    for(let i = a;i<=b;i++){
+        ans = ans +i;
+    }
 
+    return ans;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  let celsius = (fahrenheit - 32) * 5/9;
+  return celsius;
 };
 
 const convertToFahrenheit = function() {
+  let fahrenheit = celsius * 9/5 + 32;
+  return fahrenheit;
 };
 
 // Do not edit below this line
